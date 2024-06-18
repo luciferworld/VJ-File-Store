d3f397fedee25d218478cb20b5765771fc53b062
Ibrahim khan <108752662+luciferworld@users.noreply.github.com>
2024-06-18 16:40:10 +0500

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://thoughtful-rose-sungrassgroup-8b80d43f.koyeb.app/"
+        URL = "https://primitive-tarah-sungrassgroup-05725e02.koyeb.app/"
     else:
-        URL = "https://thoughtful-rose-sungrassgroup-8b80d43f.koyeb.app/"
+        URL = "https://primitive-tarah-sungrassgroup-05725e02.koyeb.app/"
 
 
 
